47fa9ee38e5cf1998061188b8a2fb900d98b6ea1
tengwanll <317260251@qq.com>
2018-03-23 15:27:34 +0800

    rabbitmq

---
+++ web/amqp_publisher.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+use Mirror\ApiBundle\Common\Constant;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$exchange = 'router';
+$queue = 'msgs';
+
+$connection = new AMQPStreamConnection(Constant::$rabbit_host, Constant::$rabbit_port, Constant::$rabbit_user, Constant::$rabbit_pass, Constant::$rabbit_vhost);
+$channel = $connection->channel();
+
+/*
+    The following code is the same both in the consumer and the producer.
+    In this way we are sure we always have a queue to consume from and an
+        exchange where to publish messages.
+*/
+
+/*
+    name: $queue
+    passive: false
+    durable: true // the queue will survive server restarts
+    exclusive: false // the queue can be accessed in other channels
+    auto_delete: false //the queue won't be deleted once the channel is closed.
+*/
+$channel->queue_declare($queue, false, true, false, false);
+
+/*
+    name: $exchange
+    type: direct
+    passive: false
+    durable: true // the exchange will survive server restarts
+    auto_delete: false //the exchange won't be deleted once the channel is closed.
+*/
+
+$channel->exchange_declare($exchange, 'direct', false, true, false);
+
+$channel->queue_bind($queue, $exchange);
+
+$messageBody = implode(' ', array_slice($argv, 1));
+echo $messageBody;
+$message = new AMQPMessage($messageBody, array('content_type' => 'text/plain', 'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT));
+$channel->basic_publish($message, $exchange);
+
+$channel->close();
+$connection->close();
