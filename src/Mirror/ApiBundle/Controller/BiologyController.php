c12273632ba82f8eb348388fa07e8ffd7923336d
zhengyue <317260251@qq.com>
2018-03-26 10:38:59 +0800

    迅搜

--- src/Mirror/ApiBundle/Controller/BiologyController.php
+++ src/Mirror/ApiBundle/Controller/BiologyController.php
@@ -88,4 +88,9 @@ class BiologyController extends BaseController
         $rr=$this->get('biology_service')->create($biology);
         return $this->buildResponse($rr);
     }
+
+    public function test(){
+        $rr=$this->get('biology_service')->getList();
+        return $this->buildResponse($rr);
+    }
 }
--- src/Mirror/ApiBundle/Controller/TestController.php
+++ src/Mirror/ApiBundle/Controller/TestController.php
@@ -25,95 +25,6 @@ use Symfony\Component\HttpFoundation\Response;
 class TestController extends BaseController
 {
     /**
-     * @Route("/consumer")
-     * @Method("GET")
-     * @return Response
-     */
-    public function consumer(){
-        $exchange = 'router';
-        $queue = 'msgs';
-        $consumerTag = 'consumer';
-
-        $connection = new AMQPStreamConnection(Constant::$rabbit_host, Constant::$rabbit_port, Constant::$rabbit_user, Constant::$rabbit_pass, Constant::$rabbit_vhost);
-        $channel = $connection->channel();
-
-        /*
-            The following code is the same both in the consumer and the producer.
-            In this way we are sure we always have a queue to consume from and an
-                exchange where to publish messages.
-        */
-
-        /*
-            name: $queue
-            passive: false
-            durable: true // the queue will survive server restarts
-            exclusive: false // the queue can be accessed in other channels
-            auto_delete: false //the queue won't be deleted once the channel is closed.
-        */
-        $channel->queue_declare($queue, false, true, false, false);
-
-        /*
-            name: $exchange
-            type: direct
-            passive: false
-            durable: true // the exchange will survive server restarts
-            auto_delete: false //the exchange won't be deleted once the channel is closed.
-        */
-
-        $channel->exchange_declare($exchange, 'direct', false, true, false);
-
-        $channel->queue_bind($queue, $exchange);
-
-        /**
-         * @param \PhpAmqpLib\Message\AMQPMessage $message
-         */
-        function process_message($message)
-        {
-            echo "\n--------\n";
-            echo $message->body;
-            echo "\n--------\n";
-
-            //确认收到消息,必须要
-            $message->delivery_info['channel']->basic_ack($message->delivery_info['delivery_tag']);
-
-            // Send a message with the string "quit" to cancel the consumer.
-            if ($message->body === 'quit') {
-                $message->delivery_info['channel']->basic_cancel($message->delivery_info['consumer_tag']);
-            }
-        }
-
-        /*
-            queue: Queue from where to get the messages
-            consumer_tag: Consumer identifier
-            no_local: Don't receive messages published by this consumer.
-            no_ack: Tells the server if the consumer will acknowledge the messages.
-            exclusive: Request exclusive consumer access, meaning only this consumer can access the queue
-            nowait:
-            callback: A PHP Callback
-        */
-
-        $channel->basic_consume($queue, $consumerTag, false, false, false, false, 'process_message');
-
-        /**
-         * @param \PhpAmqpLib\Channel\AMQPChannel $channel
-         * @param \PhpAmqpLib\Connection\AbstractConnection $connection
-         */
-        function shutdown($channel, $connection)
-        {
-            $channel->close();
-            $connection->close();
-        }
-
-        register_shutdown_function('shutdown', $channel, $connection);
-
-// Loop as long as the channel has callbacks registered
-        while (count($channel->callbacks)) {
-            $channel->wait();
-        }
-        return new Response(1);
-    }
-
-    /**
      * @Route("/publish/{message}")
      * @Method("GET")
      * @param $message
@@ -170,7 +81,7 @@ class TestController extends BaseController
         try {
             $xs=new \XS('biology');
             $search=$xs->getSearch();
-            $data=$search->search($name);
+            $data=$search->search($name);//name:基因
             var_dump($data);
         } catch (\XSException $e) {
             var_dump($e->getMessage());
